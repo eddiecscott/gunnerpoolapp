e2a5223b3bdbb718013de58c16036f97b00238cf
Eddie Scott <eddiecscott@gmail.com>
2025-01-04 18:23:37 -0600

    Changed some text

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your TEST instantly.</li>
+          <li>Save and see your TEST2 instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
